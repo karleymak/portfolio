548165a5fd0b5515ab6de14df4bc5af646723740
karleymak <karleym11@gmail.com>
2026-08-01 11:42:23 -0700

    shds & climate kit

--- script.js
+++ script.js
@@ -14,6 +14,15 @@
         overlay.classList.remove("open");
     });
 
+    
+    window.addEventListener("load", (event) => {
+        new cursoreffects.fairyDustCursor({
+            colors: ["#a7c957", "#2a9d8f", "#ef476f", '#6a4c93'],
+            fairySymbol: "★",
+          });
+    })
+    
+
 
 
 
