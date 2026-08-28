acf8098e6025a3534b953947ded2dcd1054df498
karleymak <karleym11@gmail.com>
2026-08-27 23:22:18 -0700

    update 8/27 refresh

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
     'use strict';
     console.log('reading js');
 
+    VanillaTilt.init(document.querySelectorAll(".thumbnail"), {
+        max: 8,
+        speed: 400,
+        scale: 1.02,
+        glare: true,
+        "max-glare": 0.15
+    });
+    
+
     const burger = document.querySelector(".burger");
     const overlay = document.querySelector(".nav-overlay");
     const closeBtn = document.querySelector(".close");
@@ -14,13 +23,22 @@
         overlay.classList.remove("open");
     });
 
+    document.querySelector('#tuxedo').addEventListener('mouseover', function(){
+        document.querySelector('#tuxedo').src = 'images/tuxedo2.PNG';
+    });
+
+    document.querySelector('#tuxedo').addEventListener('mouseout', function(){
+        document.querySelector('#tuxedo').src = 'images/tuxedo1.PNG';
+    });
+
+    // document.querySelector('#footer-img').addEventListener('click', function(){
+    //     document.querySelector('#footer-img').src = 'images/footer-switch.png';
+    // });
+
     
-    window.addEventListener("load", (event) => {
-        new cursoreffects.fairyDustCursor({
-            colors: ["#a7c957", "#2a9d8f", "#ef476f"],
-            fairySymbol: "★",
-          });
-    })
+
+    
+    
     
 
 
